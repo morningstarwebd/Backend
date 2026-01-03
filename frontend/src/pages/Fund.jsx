785802a9c3b014bed84087e33ae3de687e513789
morningstarwebd <morningstarwebd@gmail.com>
2026-01-04 04:34:34 +0530

    Fix toast import in Fund.jsx

--- frontend/src/pages/Fund.jsx
+++ frontend/src/pages/Fund.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import api from '../services/api';
-import toast from 'react-hot-toast';
+import { toast } from 'react-toastify';
 
 const Fund = () => {
     const [formData, setFormData] = useState({
